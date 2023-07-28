59782e713db9a88e11139cf3cdeed04327daaa43
Thisseas X <thisseasx@yahoo.com>
2023-07-29 01:59:25 +0300

    use real email

--- src/main/models/email/services.jsx
+++ src/main/models/email/services.jsx
@@ -4,18 +4,17 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { EmailOne } from 'email-templates';
 
 const transport = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
+  service: import.meta.env.MAIN_VITE_EMAIL_SERVICE,
   auth: {
-    user: 'stone.boehm@ethereal.email',
-    pass: 'wAAZNrQxr4h86gJdUy',
+    user: import.meta.env.MAIN_VITE_EMAIL_USER,
+    pass: import.meta.env.MAIN_VITE_EMAIL_PASS,
   },
 });
 
 export const sendEmail = () => {
   transport.sendMail({
     from: '"Doggo" <dog@dogs.com>',
-    to: '"Cat" <cat@cats.com',
+    to: '"Cat" <thisseasx@gmail.com>',
     subject: 'Hello Email!',
     html: renderToStaticMarkup(<EmailOne name="Cat" sender="Doggo" />),
   });
